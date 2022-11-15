c9764e081f3cc6f5a3de129c3624a1385e8afca8
Enziandom <zhengrenfu@outlook.com>
2022-11-15 21:22:31 +0800

    RecycleView 实现成功

--- Exp02-Shopping/app/src/main/java/com/example/shopping/MainActivity.java
+++ Exp02-Shopping/app/src/main/java/com/example/shopping/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.shopping;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+  private final String[] data = {"1", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2",};
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+
+    RecyclerView rv = findViewById(R.id.recycler_view);
+    CustomAdapter ca = new CustomAdapter(data);
+    rv.setAdapter(ca);
+
+    LinearLayoutManager manager = new LinearLayoutManager(MainActivity.this);
+    rv.setLayoutManager(manager);
   }
 
 }
