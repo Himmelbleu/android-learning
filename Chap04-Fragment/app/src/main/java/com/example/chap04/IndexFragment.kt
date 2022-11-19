3d0ca84c9bf0bcbf1418c2a88b392c3ca69bfc47
Enziandom <zhengrenfu@outlook.com>
2022-11-20 02:16:21 +0800

    测试 savedInstanceState

--- Chap04-Fragment/app/src/main/java/com/example/chap04/IndexFragment.kt
+++ Chap04-Fragment/app/src/main/java/com/example/chap04/IndexFragment.kt
@@ -1,8 +1,12 @@
 package com.example.chap04
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 
 class IndexFragment : Fragment(R.layout.fragment_index) {
 
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+  }
 
 }
--- Chap04-Fragment/app/src/main/java/com/example/chap04/MainActivity.kt
+++ Chap04-Fragment/app/src/main/java/com/example/chap04/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.chap04
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+
+    if (savedInstanceState == null) {
+      val bundle = Bundle()
+      bundle.putInt("param", 20);
+      supportFragmentManager
+        .beginTransaction()
+        .setReorderingAllowed(true)
+        .add(R.id.fragment_container_view, IndexFragment().javaClass, bundle, null)
+        .commit()
+    }
   }
 
 }
