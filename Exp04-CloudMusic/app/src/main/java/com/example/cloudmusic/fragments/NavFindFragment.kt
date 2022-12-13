e4bbf01394cef66b8ee0944be2043b13c3a6520e
Enziandom <zhengrenfu@outlook.com>
2022-12-13 14:19:38 +0800

    3x3 滑动布局

--- Exp04-CloudMusic/app/src/main/java/com/example/cloudmusic/fragments/NavFindFragment.kt
+++ Exp04-CloudMusic/app/src/main/java/com/example/cloudmusic/fragments/NavFindFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cloudmusic.adapters.CustSongsAdapter
@@ -21,7 +22,7 @@ class NavFindFragment : Fragment() {
   private lateinit var recoSongsAdapter: RecoSongsAdapter
   private lateinit var custSongsAdapter: CustSongsAdapter
 
-  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
     binding = FragmentNavFindBinding.inflate(layoutInflater, container, false)
     if (savedInstanceState == null) {
       createBanner()
@@ -40,11 +41,11 @@ class NavFindFragment : Fragment() {
           SlideShowItem("https://p1.music.126.net/UuSd71D8bonCDUJYdp7avQ==/109951168139528689.jpg?imageView&quality=89")
         ), this
       )
-    ).indicator = CircleIndicator(context);
+    ).indicator = CircleIndicator(context)
   }
 
   private fun createRecoSongs() {
-    binding.recoSongsRecyclerView.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, true)
+    binding.recoSongsRecyclerView.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
     recoSongsAdapter = RecoSongsAdapter(
       arrayListOf(
         RecoSongsItem("https://p2.music.126.net/SyerXTn8Np6646rabTS8EA==/109951163046604560.jpg?param=130y130", "Funny Logic"),
@@ -57,14 +58,18 @@ class NavFindFragment : Fragment() {
   }
 
   private fun createCustSongs() {
-    binding.custSongsRecyclerView.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, true)
+    binding.custSongsRecyclerView.layoutManager = GridLayoutManager(requireContext(), 3, RecyclerView.HORIZONTAL, false)
     custSongsAdapter = CustSongsAdapter(
       arrayListOf(
-        CustSongsItem("https://p2.music.126.net/SyerXTn8Np6646rabTS8EA==/109951163046604560.jpg?param=130y130", "Funny Logic", "Author"),
-        CustSongsItem("https://p2.music.126.net/SyerXTn8Np6646rabTS8EA==/109951163046604560.jpg?param=130y130", "Funny Logic", "Author"),
-        CustSongsItem("https://p2.music.126.net/SyerXTn8Np6646rabTS8EA==/109951163046604560.jpg?param=130y130", "Funny Logic", "Author"),
-        CustSongsItem("https://p2.music.126.net/SyerXTn8Np6646rabTS8EA==/109951163046604560.jpg?param=130y130", "Funny Logic", "Author"),
-        CustSongsItem("https://p2.music.126.net/SyerXTn8Np6646rabTS8EA==/109951163046604560.jpg?param=130y130", "Funny Logic", "Author"),
+        CustSongsItem("https://p2.music.126.net/SyerXTn8Np6646rabTS8EA==/109951163046604560.jpg?param=130y130", "Funny Logic1", "Author"),
+        CustSongsItem("https://p2.music.126.net/SyerXTn8Np6646rabTS8EA==/109951163046604560.jpg?param=130y130", "Funny Logic2", "Author"),
+        CustSongsItem("https://p2.music.126.net/SyerXTn8Np6646rabTS8EA==/109951163046604560.jpg?param=130y130", "Funny Logic3", "Author"),
+        CustSongsItem("https://p2.music.126.net/SyerXTn8Np6646rabTS8EA==/109951163046604560.jpg?param=130y130", "Funny Logic4", "Author"),
+        CustSongsItem("https://p2.music.126.net/SyerXTn8Np6646rabTS8EA==/109951163046604560.jpg?param=130y130", "Funny Logic5", "Author"),
+        CustSongsItem("https://p2.music.126.net/SyerXTn8Np6646rabTS8EA==/109951163046604560.jpg?param=130y130", "Funny Logic6", "Author"),
+        CustSongsItem("https://p2.music.126.net/SyerXTn8Np6646rabTS8EA==/109951163046604560.jpg?param=130y130", "Funny Logic7", "Author"),
+        CustSongsItem("https://p2.music.126.net/SyerXTn8Np6646rabTS8EA==/109951163046604560.jpg?param=130y130", "Funny Logic8", "Author"),
+        CustSongsItem("https://p2.music.126.net/SyerXTn8Np6646rabTS8EA==/109951163046604560.jpg?param=130y130", "Funny Logic9", "Author"),
       ), requireContext()
     )
     binding.custSongsRecyclerView.adapter = custSongsAdapter
