92f7c0023864e4d4faf4df53dba4519498398ed5
Enziandom <zhengrenfu@outlook.com>
2022-12-14 00:20:05 +0800

    暂停、开始播放功能已完成

--- Exp04-CloudMusic/app/src/main/java/com/example/cloudmusic/PlayerActivity.java
+++ Exp04-CloudMusic/app/src/main/java/com/example/cloudmusic/PlayerActivity.java
@@ -2,8 +2,10 @@ package com.example.cloudmusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
@@ -13,9 +15,13 @@ import com.example.cloudmusic.databinding.ActivityPlayerBinding;
 
 import java.io.IOException;
 
+import jp.wasabeef.glide.transformations.BlurTransformation;
+
 public class PlayerActivity extends AppCompatActivity {
   private final MediaPlayer player = new MediaPlayer();
   private ActivityPlayerBinding binding;
+
+  private boolean isDown = false;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +32,26 @@ public class PlayerActivity extends AppCompatActivity {
     Song song = (Song) getIntent().getExtras().getSerializable("song");
     binding.text.setText(song.getText());
     binding.author.setText(song.getAuthor());
-    RequestOptions options = RequestOptions.bitmapTransform(new CircleCrop());
-    Glide.with(this).load(song.getSurface()).apply(options).into(binding.surface);
+    Glide.with(this).load(song.getSurface())
+      .apply(RequestOptions.bitmapTransform(new CircleCrop()))
+      .into(binding.surface);
+    Glide.with(this).load(song.getSurface())
+      .apply(RequestOptions.bitmapTransform(new BlurTransformation(70, 4)))
+      .into(binding.bg);
+
     initPlayer(song);
+
+    binding.ctrlBtn.setOnClickListener(v -> {
+      if (isDown) {
+        player.pause();
+        binding.ctrlBtn.setImageResource(R.drawable.ic_play_circle);
+        isDown = false;
+      } else {
+        player.start();
+        binding.ctrlBtn.setImageResource(R.drawable.ic_pause_circle);
+        isDown = true;
+      }
+    });
   }
 
   private void initPlayer(Song song) {
@@ -37,6 +60,7 @@ public class PlayerActivity extends AppCompatActivity {
       player.prepareAsync();
       player.setOnPreparedListener(mp -> {
         player.start();
+        binding.ctrlBtn.setImageResource(R.drawable.ic_pause_circle);
       });
     } catch (IOException e) {
       e.printStackTrace();
